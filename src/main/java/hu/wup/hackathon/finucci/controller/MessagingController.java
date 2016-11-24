93e3a768f57af2e6644b115e5a1cf9da6cab2c66
Zoltán Gábriel <zoltan.gabriel@wup.hu>
2016-11-24 21:16:33 +0100

    message processing

--- src/main/java/hu/wup/hackathon/finucci/controller/MessagingController.java
+++ src/main/java/hu/wup/hackathon/finucci/controller/MessagingController.java
@@ -1,5 +1,7 @@
 package hu.wup.hackathon.finucci.controller;
 
+import hu.wup.hackathon.finucci.model.MessengerMessage;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,15 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessagingController {
     
     private static final String VERIFY_TOKEN = "F.inucci";
+    private static final String REQUEST_OBJECT_PAGE = "page";
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
-    String home(@RequestParam("hub.verify_token") String verifyToken, @RequestParam("hub.challenge") String challenge) {
+    String registerWebhook(@RequestParam("hub.verify_token") String verifyToken, @RequestParam("hub.challenge") String challenge) {
         if (VERIFY_TOKEN.equals(verifyToken)) {
             return challenge;
         } else {
             return "Error, wrong validation token.";
         }
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
+    String receiveMessage(@RequestBody MessengerMessage messengerMessage) {
+        if (REQUEST_OBJECT_PAGE.equals(messengerMessage.getObject())) {
+            messengerMessage.getEntry().forEach((entry) -> {
+                System.out.println("Received message: " + entry.getMessaging().getMessage());
+            });
+            
+        }
+        return "OK";
     }
 
 }
