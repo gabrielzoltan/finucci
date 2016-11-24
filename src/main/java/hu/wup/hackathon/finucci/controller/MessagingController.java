481d502d57b4d521cdb4220dd59e51245f38cf2c
Zoltán Gábriel <zoltan.gabriel@wup.hu>
2016-11-24 21:49:38 +0100

    Implemented response sending.

--- src/main/java/hu/wup/hackathon/finucci/controller/MessagingController.java
+++ src/main/java/hu/wup/hackathon/finucci/controller/MessagingController.java
@@ -1,12 +1,20 @@
 package hu.wup.hackathon.finucci.controller;
 
+import hu.wup.hackathon.finucci.model.Message;
+import hu.wup.hackathon.finucci.model.Messaging;
 import hu.wup.hackathon.finucci.model.MessengerMessage;
+import hu.wup.hackathon.finucci.model.Recipient;
+import hu.wup.hackathon.finucci.model.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MessagingController {
@@ -30,12 +38,25 @@ public class MessagingController {
         if (REQUEST_OBJECT_PAGE.equals(messengerMessage.getObject())) {
             messengerMessage.getEntry().forEach((entry) -> {
                 entry.getMessaging().forEach((messaging) -> {
-                    System.out.println("Received message: " + messaging.getMessage());
+                    System.out.println("Received message: " + messaging.getMessage().getText());
+                    sendReply(messaging.getSender().getId(), messaging.getMessage().getText());
                 });
             });
-            
         }
         return "OK";
+    }
+    
+    private void sendReply(String recipientId, String receivedMessage) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("access_token", "EAAQZBzHgrghUBADdHowZBkhN8zdjtQDBfZARnh9WQXUcsPK6nA2thEx219PzbdgVgNW9vOZCiZArNpJ405KQVvaZCER2yKZACEsnZAFMZCfMCZAuUASawgDg4q7TZA6EvAZARnYZBnRfXKMOIgbYre5LgLdvyM1zcZAH5jJ1CK5YhEkoH0MQZDZD");
+        RestTemplate restTemplate = new RestTemplate();
+        Messaging response = new Messaging();
+        Recipient recipient = new Recipient();
+        recipient.setId(recipientId);
+        Message message = new Message();
+        message.setText(receivedMessage);
+        ResponseEntity<Response> resp = restTemplate.postForEntity("https://graph.facebook.com/v2.6/me/messages", response, Response.class, uriVariables);
+        System.out.println("Response status: " + resp.getStatusCode());
     }
 
 }
---
+++ src/main/java/hu/wup/hackathon/finucci/model/Response.java
@@ -0,0 +1,27 @@
+package hu.wup.hackathon.finucci.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Response {
+    
+    @JsonProperty("recipient_id")
+    private String recipientId;
+    @JsonProperty("message_id")
+    private String messageId;
+
+    public String getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(String recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+}
